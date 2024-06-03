a8d4512699a95bf23b9d4fc24cb7e36d06e0023b
Jaswanth Manda <jaswanth@true-fan.in>
2024-06-03 12:05:06 +0530

    added code to create db

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from router import section5p1, section5p2
 from fastapi import FastAPI
-
+from db import models
+from db.database import engine
 
 app = FastAPI()
 app.include_router(section5p1.router)
@@ -10,3 +11,6 @@ app.include_router(section5p2.router)
 @app.get('/hello')
 def index():
     return 'Hello World!'
+
+
+models.Base.metadata.create_all(engine)
